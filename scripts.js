d8db8c912cdb7067b7679c1a7e285c4b7e524cb1
rashmi2203 <rashmihukkeri6@gmail.com>
2024-09-01 09:16:34 +0530

    added url

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const msalConfig = {
     auth: {
         clientId: '8d2a547f-27d9-46ee-8a09-7f9f7e951dd3', // Replace with your Azure AD app's client ID
         authority: 'https://login.microsoftonline.com/64465d85-01e4-4334-957c-b6acd6361647', // Replace with your tenant ID
-        redirectUri: 'https://front-akeqf7f9ahg9drd7.eastasia-01.azurewebsites.net.azurewebsites.net', // Replace with your app service URL
+        redirectUri: 'https://front-akeqf7f9ahg9drd7.eastasia-01.azurewebsites.net', // Replace with your app service URL
     }
 };
 
